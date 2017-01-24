64e6b5d2d9ec2aa2e698e5c59333d39402648b2c
Andy Arvanitis <andyarvanitis@gmail.com>
2017-01-23 20:14:16 -0800

    C++: fix FFI

--- src/Data/Lazy.cc
+++ src/Data/Lazy.cc
@@ -13,24 +13,39 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
 
+#include <thread>
+#include <mutex>
 #include "Lazy.hh"
 
 namespace Data_Lazy {
 
   using namespace PureScript;
 
+  class Deferred {
+    public:
+      Deferred(const any& f) : thunk(f), value(nullptr) {}
+      Deferred() = delete;
+
+      auto force() -> const any& {
+        std::call_once(flag, [&](){ value = thunk(unit); });
+        return value;
+      }
+    private:
+      std::once_flag flag;
+      const any thunk;
+      any value;
+  };
+
   // foreign import defer :: forall a. (Unit -> a) -> Lazy a
   //
   auto defer(const any& f) -> any {
-    return [=]() -> any {
-      static const any the_value = f(unit);
-      return the_value;
-    };
+    return make_managed<Deferred>(f);
   }
 
   // foreign import force :: forall a. Lazy a -> a
   //
   auto force(const any& l) -> any {
-    return l();
+    return cast<Deferred>(l).force();
   }
+
 }
